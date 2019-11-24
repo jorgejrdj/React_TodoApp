404e3d096bbf44c44e549213f9905f09a04fa539
Jorge de Souza <jorgejrdj1@gmail.com>
2019-11-24 20:04:44 -0300

    alterar para startar

--- src/config/server.js
+++ src/config/server.js
@@ -1,13 +1,15 @@
 const port = 3003
 
-const bodyParser = require ('body-parser')
-const express = require ('express')
+const bodyParser = require('body-parser')
+const express = require('express')
 const server = express()
 
 //middleware
-server.use(bodyParser.urlenconded({extended: true}))
+server.use(bodyParser.urlenconded({ extended: true }))
 server.use(bodyParser.json())
 
-server.listen(port,function(){
+server.listen(port, function() {
     console.log(`Backend is running on port ${port}.`)
 })
+
+module.exports = server
