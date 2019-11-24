7aa61cf9bf2bf115937b4728410ed4605aaad2d4
Jorge de Souza <jorgejrdj1@gmail.com>
2019-11-24 20:12:54 -0300

    alterar para startar 2

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
-moongose.Promise = global.Promise
-module.exports = moongose.connect('mongodb://localhost/todo')
+mongoose.Promise = global.Promise
+module.exports = mongoose.connect('mongodb://localhost/todo')
--- src/config/server.js
+++ src/config/server.js
@@ -5,7 +5,7 @@ const express = require('express')
 const server = express()
 
 //middleware
-server.use(bodyParser.urlenconded({ extended: true }))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
 server.listen(port, function() {
